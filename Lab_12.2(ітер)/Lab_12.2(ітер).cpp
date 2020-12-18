5263723a21a234f03ff74d6331b9171b00c20081
Woloda <dorozovecvoloda@gmail.com>
2020-12-18 12:33:47 +0200

    Lab__12_2_iter

---
+++ Lab_12.2(ітер)/Lab_12.2(ітер).cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+typedef int inform;
+
+struct Spusok
+{
+	Spusok* link1;
+	inform inf;
+};
+
+void Creata(Spusok*& first, Spusok*& last, inform inf);
+void COUT(Spusok*& T);
+void Process(Spusok*& T, int x);
+
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Spusok* first = NULL;
+	Spusok* last = NULL;
+
+	for (int i = 0; i < 4; i++)
+		Creata(first, last, i);
+
+	Spusok* T = first;
+	COUT(T);
+	T = first;
+
+	int x;
+	cout << "Введіть певне значення, для збільшення величини інформаційного поля кожного елемента: ";
+	cin >> x;
+	Process(T, x);
+
+	return 0;
+}
+
+void Creata(Spusok*& first, Spusok*& last, inform inf)
+{
+	Spusok* tmp = new Spusok;
+
+	tmp->inf = inf;
+	tmp->link1 = NULL;
+	if (first == NULL)
+		first = tmp;
+	else
+		last->link1 = tmp;
+	last = tmp;
+}
+
+void COUT(Spusok*& T)
+{
+	inform inf;
+
+	cout << "Вивід інформації про кожен сформований елемент: " << endl << endl;
+	while (T != NULL)
+	{
+		inf = T->inf;
+		cout << inf << "   ";
+		T = T->link1;
+	}
+	cout << endl << endl;
+}
+
+void Process(Spusok*& T, int x)
+{
+	inform inf;
+
+	cout << endl;
+	cout << "Вивід інформації про кожен модифікований елемент " << endl << endl;
+	while (T != NULL)
+	{
+		inf = T->inf;            // 1(відбувається копіювання значення елемента з списку в змінну 'inf')
+		T->inf = inf + x;        // 2(запис модфікованого значення(збільшене на 'x') в елемент списку)
+		cout << T->inf << "   ";
+		T = T->link1;            // 3(перехід вказівника на наступну позицію для виконання операцій з наступним елемнтом)
+	}
+	cout << endl;
+}
---
+++ UnitTest(Lab_12.2(ітер.))/UnitTest(Lab_12.2(ітер.)).cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_12.2(ітер)/Lab_12.2(ітер).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab122ітер
+{
+	TEST_CLASS(UnitTestLab122ітер)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Spusok* first = NULL;
+			Spusok* last = NULL;
+			for (int i = 0; i < 2; i++)
+				Creata(first, last, i);
+
+			Spusok* T = first;
+			int x = 3;
+			Process(T, x);
+
+			T = first;
+			int l = T->inf;
+			Assert::AreEqual(l, 3);
+		}
+	};
+}
